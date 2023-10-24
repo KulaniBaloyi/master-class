37105eb74859f9a9878d783ae7f67ffa8e7a6802
KulaniBaloyi <kulani17.kb@gmail.com>
2023-10-25 00:44:32 +0200

    added slider functionality

--- app/components/ComingSoon.js
+++ app/components/ComingSoon.js
@@ -1,28 +1,38 @@
-import React from 'react'
+"use client"
 
 const ComingSoon = () => {
+    const slideLeft = () => {
+        var slider = document.getElementById('videoSlider');
+        slider.scrollLeft = slider.scrollLeft - 500;
+      };
+    
+      const slideRight = () => {
+        var slider = document.getElementById('videoSlider');
+        slider.scrollLeft = slider.scrollLeft + 500;
+      };
   return (
     <div className='ml-[2.5%]'>
         <h2 className='text-3xl font-semibold leading-8'>Coming Soon</h2>
         <section className=' flex flex-col relative'>
 
-    <div className='w-full h-[500px] flex gap-5 overflow-x-hidden pr-5 items-center '>
-        <div className='h-[300px] max-w-full aspect-video border rounded-xl shrink-0'></div>
-        <div className='h-[300px] max-w-full aspect-video border rounded-xl shrink-0'></div>
-        <div className='h-[300px] max-w-full aspect-video border rounded-xl shrink-0'></div>
-        <p className='absolute right-5 top-1/2 -translate-y-1/2 rounded-full bg-[#9ea0a9] grid place-content-center p-2 '>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-white">
-  <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z" clipRule="evenodd" />
-</svg>
-
-        </p>
-        <p className='hidden absolute left-0 top-1/2 -translate-y-1/2 rounded-full bg-[#9ea0a9] place-content-center p-2 '>
+    <div id="videoSlider" className='w-full h-[500px] flex gap-5 overflow-x-scroll scrollbar-hide pr-5 items-center '>
+        <div className='h-[300px] max-w-full aspect-video border rounded-xl shrink-0 snap-center'></div>
+        <div className='h-[300px] max-w-full aspect-video border rounded-xl shrink-0 snap-center'></div>
+        <div className='h-[300px] max-w-full aspect-video border rounded-xl shrink-0 snap-center'></div>
+        
+        <button onClick={slideLeft} className='absolute left-0 top-1/2 -translate-y-1/2 rounded-full bg-[#9ea0a9] place-content-center p-2 '>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-white">
   <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z" clipRule="evenodd" />
 </svg>
 
 
-        </p>
+        </button>
+        <button onClick={slideRight} className='absolute right-5 top-1/2 -translate-y-1/2 rounded-full bg-[#9ea0a9] grid place-content-center p-2 '>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-white">
+  <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z" clipRule="evenodd" />
+</svg>
+
+        </button>
         
         
     </div>
--- app/components/Explore.js
+++ app/components/Explore.js
@@ -1,6 +1,16 @@
-import React from 'react'
+"use client"
 
 const Explore = () => {
+    const slideLeft = () => {
+        var slider = document.getElementById('slider');
+        slider.scrollLeft = slider.scrollLeft - 500;
+      };
+    
+      const slideRight = () => {
+        var slider = document.getElementById('slider');
+        slider.scrollLeft = slider.scrollLeft + 500;
+      };
+     
   return (
     <div className='my-10 flex flex-col'>
         <h2 className='font-semibold text-center text-5xl lg:w-3/5 mx-auto  my-5'>A dose of inspiration,
@@ -23,16 +33,16 @@ whenever you need it.</h2>
     <h2 className='text-lg leading-8 font-semibold'>Popular Now <span className='text-gray-500'>See All</span></h2>
    
 <div className='flex gap-2'>
-    <p className='text-white bg-[#9ea0a9] aspect-square rounded-full p-1 grid place-content-center'>
+    <button onClick={slideLeft} className='text-white bg-[#9ea0a9] aspect-square rounded-full p-1 grid place-content-center'>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
   <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
 </svg>
-    </p>
-    <p className='text-white bg-[#9ea0a9] aspect-square rounded-full p-1 grid place-content-center'>
+    </button>
+    <button onClick={slideRight} className='text-white bg-[#9ea0a9] aspect-square rounded-full p-1 grid place-content-center'>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
   <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
 </svg>
-    </p>
+    </button>
 
 
 
@@ -40,7 +50,7 @@ whenever you need it.</h2>
 
     </div>
    
-    <div className='w-full h-[500px] flex gap-5 overflow-x-scroll py-5 snap-x'>
+    <div id='slider' className='w-full h-[500px] flex gap-5 overflow-x-scroll scroll-smooth scrollbar-hide py-5 snap-x'>
         <div className='w-[256px] h-full aspect[9/16] border rounded-lg shrink-0 snap-center'></div>
         <div className='w-[256px] h-full aspect[9/16] border rounded-lg shrink-0 snap-center'></div>
         <div className='w-[256px] h-full aspect[9/16] border rounded-lg shrink-0 snap-center'></div>
